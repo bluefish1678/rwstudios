12f738718c2eb2d8ded5a1b5d3322718f9de1492
Glitch (glitch-hello-website) <none>
2022-05-14 19:35:00 +0000

    🎎🏂 Checkpoint ./index.html:41104452/248 ./script.js:41104452/6934 ./style.css:41104452/4234

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
-document.addEventListener('DOMContentLoaded',function(event){
-  // array with texts to type in typewriter
-  var dataText = [ "Utrecht.", "Full Service.", "Webdevelopment.", "Wij zijn Codefield!"];
-  
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
-  function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
-    if (i < (text.length)) {
-      // add next character to h1
-     document.querySelector("h6").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+// List of sentences
+var _CONTENT = [
+  "Twinkle, twinkle, little star",
+  "How I wonder what you are",
+  "Up above the world so high",
+  "Like a diamond in the sky",
+];
 
-      // wait for a while and call this function again for next character
-      setTimeout(function() {
-        typeWriter(text, i + 1, fnCallback)
-      }, 100);
-    }
-    // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == 'function') {
-      // call callback after timeout
-      setTimeout(fnCallback, 700);
-    }
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Cursor element
+var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() {
+  // Get substring with 1 characater added
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX++;
+
+  // If full sentence has been displayed then start to delete the sentence after some time
+  if (text === _CONTENT[_PART]) {
+    // Hide the cursor
+    _CURSOR.style.display = "none";
+
+    clearInterval(_INTERVAL_VAL);
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(Delete, 50);
+    }, 1000);
   }
-  // start a typewriter animation for a text in the dataText array
-   function StartTextAnimation(i) {
-     if (typeof dataText[i] == 'undefined'){
-        setTimeout(function() {
-          StartTextAnimation(0);
-        }, 20000);
-     }
-     // check if dataText[i] exists
-    if (i < dataText[i].length) {
-      // text exists! start typewriter animation
-     typeWriter(dataText[i], 0, function(){
-       // after callback (and whole text has been animated), start next text
-       StartTextAnimation(i + 1);
-     });
-    }
+}
+
+// Implements deleting effect
+function Delete() {
+  // Get substring with 1 characater deleted
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX--;
+
+  // If sentence has been deleted then start to display the next sentence
+  if (text === "") {
+    clearInterval(_INTERVAL_VAL);
+
+    // If current sentence was last then display the first one, else move to the next
+    if (_PART == _CONTENT.length - 1) _PART = 0;
+    else _PART++;
+
+    _PART_INDEX = 0;
+
+    // Start to display the next sentence after some time
+    setTimeout(function () {
+      _CURSOR.style.display = "inline-block";
+      _INTERVAL_VAL = setInterval(Type, 100);
+    }, 200);
   }
-  // start the text animation
-  StartTextAnimation(0);
-});
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
